ffe0fdc4ca1df738e5ffdc1a1d1c73ac7c589739
Tahaak67 <taha699@gmail.com>
2024-09-12 20:17:15 +0200

    Add Image loading from internet

--- app/src/main/java/ly/com/tahaben/android_tutorials/presentation/posts/PostsScreen.kt
+++ app/src/main/java/ly/com/tahaben/android_tutorials/presentation/posts/PostsScreen.kt
@@ -3,6 +3,9 @@ package ly.com.tahaben.android_tutorials.presentation.posts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -15,6 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import ly.com.tahaben.android_tutorials.domain.model.PostItem
 
 /* Created by Taha https://github.com/tahaak67/ at 12/9/2024 */
 
@@ -42,15 +47,28 @@ fun PostsScreen(
             }
         }
         items(state.postsList) { post ->
-            Column(
-                Modifier.fillParentMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                Text(post.title, fontSize = 25.sp, fontWeight = FontWeight.Bold)
-                Text(post.content, fontSize = 16.sp, fontWeight = FontWeight.Normal)
-                HorizontalDivider()
-            }
+            PostListItem(post = post)
         }
     }
 
+}
+
+@Composable
+fun PostListItem(modifier: Modifier = Modifier, post: PostItem) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        // Loading an Image from the internet
+        AsyncImage(
+            model = post.picture, // image URL
+            contentDescription = post.title,
+            modifier = Modifier.size(width = 200.dp, height = 100.dp)
+        )
+        Text(post.title, fontSize = 25.sp, fontWeight = FontWeight.Bold)
+        Text(post.content, fontSize = 16.sp, fontWeight = FontWeight.Normal)
+        HorizontalDivider()
+    }
 }
